1fa3ba9484a5b1b8c20b0e06c6054f6ff999a689
Josh Ellington <josh.ellington@freshconsulting.com>
2021-09-19 02:02:28 -0700

    added cfront host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,4 +114,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.lograge.enabled = true
+
+  config.hosts << "d1hxhxtmy19k6c.cloudfront.net"
 end
